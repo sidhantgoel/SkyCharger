9709a58bb2027c53dac0202b3f6d58a61f65bb0e
Sidhant Goel <sidhant@freo.money>
2026-03-31 15:19:28 +0530

    Enhance OSX certificate script to allow both codesigning and productbuild by adding the productbuild tool to the import commands.

--- add-osx-cert.sh
+++ add-osx-cert.sh
@@ -17,8 +17,8 @@ security default-keychain -s $KEY_CHAIN
 # Unlock the keychain
 security unlock-keychain -p actions $KEY_CHAIN
 
-security import $APPLICATION_CERTIFICATE_P12 -k $KEY_CHAIN -P $CERTIFICATE_PASSWORD -T /usr/bin/codesign;
-security import $INSTALLER_CERTIFICATE_P12 -k $KEY_CHAIN -P $CERTIFICATE_PASSWORD -T /usr/bin/codesign;
+security import $APPLICATION_CERTIFICATE_P12 -k $KEY_CHAIN -P $CERTIFICATE_PASSWORD -T /usr/bin/codesign -T /usr/bin/productbuild;
+security import $INSTALLER_CERTIFICATE_P12 -k $KEY_CHAIN -P $CERTIFICATE_PASSWORD -T /usr/bin/codesign -T /usr/bin/productbuild;
 
 security set-key-partition-list -S apple-tool:,apple: -s -k actions $KEY_CHAIN
 
